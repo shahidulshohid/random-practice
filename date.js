bd8c9cc891b77e8a4c0b5f3ad22d1d9bf5696c21
shahidul shohid <shahidulislamshohid7gmail.com>
2024-10-13 21:23:38 +0600

    new date practice is something is done

--- date.js
+++ date.js
@@ -1,1 +1,24 @@
-console.log('shahidul islam')
+// const date = new Date().getDate()
+// const date = new Date().toLocaleString()
+// console.log(date)
+//===============================
+// const year = new Date().getFullYear()
+// console.log(year)
+//=====================================
+// const month = new Date().getMonth() + 1
+// console.log(month)
+//==========================
+// const day = new Date().getDay()
+// const dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "saturday"]
+// console.log(dayOfWeek[day])
+//==============================
+// const time = new Date().getTime()
+// console.log(time)
+//============================
+// const startTime = new Date().getTime()
+// const endTime = new Date().getTime()
+// const timeElapsed = endTime - startTime;
+// console.log(timeElapsed)
+//================================
+const hours = new Date().getHours() 
+console.log(hours)
